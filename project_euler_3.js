da549b1753888914a926f5ec594bf978f5776596
ZACKVS <zvs@live.co.uk>
2015-01-03 07:11:11 +0000

    all working

--- project_euler_3.js
+++ project_euler_3.js
@@ -1,22 +1,30 @@
-var a = 1000;
+var a = 13195;
 var factors = [];
 var prime = [];
+var dick = [];
 
 for(var i = 0; i<=a; i++){
 	if (a % i === 0){
 		factors.push(i);
-		for(var e = 0; e<= Math.sqrt(i); e++){
+		for(var e = 0; e <= i; e++){
 			if(i % e === 0){
 				prime.push(e);
+			}
+			if(i===e){
+				if(prime.length ===2){
+					dick.push(i);
+				}
+				prime = [];
 			}
 	}
 }
 }
 
-var largest = Math.max.apply(Math, prime);
+var largest = Math.max.apply(Math, dick);
 
 
 
 console.log(factors);
 console.log(prime);
+console.log(dick);
 console.log(largest);
