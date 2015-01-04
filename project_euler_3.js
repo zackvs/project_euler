a9c50f250c58fb48b4553eb775dfdb7b4689eacb
ZACKVS <zvs@live.co.uk>
2015-01-04 09:30:17 +0000

    cleaner soln to 3 with some changes to exception answers

--- project_euler_3.js
+++ project_euler_3.js
@@ -3,28 +3,29 @@ var factors = [];
 var prime = [];
 var dick = [];
 
-for(var i = 0; i<=a; i++){
-	if (a % i === 0){
-		factors.push(i);
-		for(var e = 0; e <= i; e++){
-			if(i % e === 0){
-				prime.push(e);
-			}
-			if(i===e){
-				if(prime.length ===2){
-					dick.push(i);
-				}
-				prime = [];
-			}
-	}
-}
+for (var i = 0; i <= a; i++) {
+    if (a % i === 0) {
+        factors.push(i);
+
+        for (var e = 0; e <= i; e++) {
+            if (i % e === 0) {
+                prime.push(e);
+            }
+
+            if (i === e) {
+                if (prime.length === 2) {
+                    dick.push(i);
+                }
+                prime = [];
+            }
+        }
+    }
 }
 
 var largest = Math.max.apply(Math, dick);
 
-
-
-console.log(factors);
-console.log(prime);
-console.log(dick);
-console.log(largest);
+if (a === 0 || a === 1) {
+    console.log("No Primes");
+} else {
+    console.log("answer = " + largest);
+}
